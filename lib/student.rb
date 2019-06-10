58887206721344d2e492eeeb8086f7802e3f9aba
github username <github email address>
2019-06-10 10:06:31 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,74 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
     # create a new Student object given a row from the database
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
+
+  def self.all_students_in_grade_9
+    self.all.select do |student|
+      student.grade == "9"
+    end
+  end
+
+  def self.students_below_12th_grade
+    self.all.select do |student|
+      student.grade < "12"
+    end
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    self.all.select do |student|
+      student.grade == "10"
+    end[0...num]
+  end
+
+  def self.first_student_in_grade_10
+    self.all.select do |student|
+      student.grade == "10"
+    end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    self.all.select do |student|
+      student.grade == "#{grade}"
+    end
+  end
+
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
+
   
   def save
     sql = <<-SQL
@@ -29,8 +84,7 @@ class Student
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT
-    )
+      grade TEXT)
     SQL
 
     DB[:conn].execute(sql)
